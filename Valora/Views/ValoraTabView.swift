4e9aeb73fb91229f62d53aacb80b5fe40af4a9a1
Arkaprava Ghosh <arkapravaghosh99@gmail.com>
2024-09-20 18:29:05 +0530

    Add missing tab view.

--- Valora/Views/ValoraTabView.swift
+++ Valora/Views/ValoraTabView.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct ValoraTabView : View {
+    var body: some View {
+        NavigationStack {
+            TabView {
+                PasscodeListView()
+                    .tabItem {
+                        Image(systemName: "list.clipboard")
+                            .renderingMode(.template)
+                    }
+                SettingsView()
+                    .tabItem {
+                        Image(systemName: "gear")
+
+                            .renderingMode(.template)
+                    }
+            }
+        }
+    }
+}
